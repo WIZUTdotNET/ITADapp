10a03c90ac9135887745005f503172b6e4b70594
Sebastian Ratańczuk <46678414+Ictis0@users.noreply.github.com>
2021-05-05 18:53:45 +0200

    Adds lecture CRUD

---
+++ src/main/java/pl/dotnet/main/dto/CreateLectureDTO.java
@@ -0,0 +1,26 @@
+package pl.dotnet.main.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateLectureDTO {
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private Instant startDate;
+
+    @NotNull
+    private Long eventId;
+}
