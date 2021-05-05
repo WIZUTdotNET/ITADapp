0f4f02b6a20d9cd7309a6569846b97f98a6aad31
Sebastian Ratańczuk <46678414+Ictis0@users.noreply.github.com>
2021-05-05 13:02:32 +0200

    Remove unused class

--- src/main/java/pl/dotnet/main/expections/DuplitatedEntityExpection.java
+++
@@ -1,9 +0,0 @@
-package pl.dotnet.main.expections;
-
-public class DuplitatedEntityExpection extends RuntimeException {
-
-    public DuplitatedEntityExpection(String message) {
-        super(message);
-    }
-
-}
