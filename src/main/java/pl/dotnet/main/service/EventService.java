b02a72e84b897acda51835d051064977744a2864
Sebastian Ratańczuk <46678414+Ictis0@users.noreply.github.com>
2021-05-19 23:38:09 +0200

    Code reformat

--- src/main/java/pl/dotnet/main/service/EventService.java
+++ src/main/java/pl/dotnet/main/service/EventService.java
@@ -48,18 +48,7 @@ public class EventService {
 
     public DetailedEventDTO findById(Long id) {
         Event event = eventRepository.findById(id).orElseThrow(() -> new NotFoundRequestException("Event not found"));
-        DetailedEventDTO eventDTO = eventMapper.eventToDetailedDto(event);
-//        try {
-//            User user = userService.getCurrentUser();
-//            Set<Ticket> ticket = event.getRegisteredUsers().stream()
-//                    .filter(l -> l.getUser().equals(user))
-//                    .collect(Collectors.toSet());
-//
-//            eventDTO.setCurrentUserRegistered(!ticket.isEmpty());
-//        }catch (NotFoundRequestException e){
-//            eventDTO.setCurrentUserRegistered(false);
-//        }
-        return eventDTO;
+        return eventMapper.eventToDetailedDto(event);
     }
 
     public List<EventDTO> findByName(String name) {
